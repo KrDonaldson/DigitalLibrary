9912d36b2cbea2b70ea2438db91ee2e6113a4419
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-08-22 14:21:33 -0600

    login and register pages successfully loaded

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from .site.routes import site
 from config import Config
+from .site.routes import site
+from .auth.routes import auth
 
 app = Flask(__name__)
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
--- app/site/routes.py
+++ app/site/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template
-from flask import Blueprint, render_template
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
