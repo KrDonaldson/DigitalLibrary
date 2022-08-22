3ca06435ad242aeed96dc5d03ac368e0dccd1a43
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-08-22 13:34:26 -0600

    added routes for home page

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from .site.routes import site
+
+app.register_blueprint(site)
---
+++ app/site/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
